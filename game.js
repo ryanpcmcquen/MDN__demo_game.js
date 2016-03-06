bc3c75383ff0df2b335b2e31cb912d9682f648eb
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-06 14:03:03 -0800

    middle of step 2/10
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- game.js
+++ game.js
@@ -5,22 +5,24 @@
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
 
-  ctx.beginPath();
-  ctx.rect(20, 40, 50, 50);
-  ctx.fillStyle = "#ff0000";
-  ctx.fill();
-  ctx.closePath();
+  var x = canvas.width / 2;
+  var y = canvas.height - 30;
+  var dx = 2;
+  var dy = -2;
 
-  ctx.beginPath();
-  ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-  ctx.fillStyle = "green";
-  ctx.fill();
-  ctx.closePath();
+  var draw = function () {
+    // this clears the frame
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  ctx.rect(160, 10, 100, 40);
-  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-  ctx.stroke();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095dd";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+  };
+
+  setInterval(draw, 10);
 
 }());
