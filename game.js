c5dd8374ea3d8359e5aeb61102402dda16773779
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-06 14:37:06 -0800

    look at the moving ball
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- game.js
+++ game.js
@@ -6,19 +6,22 @@
   var ctx = canvas.getContext("2d");
 
   var x = canvas.width / 2;
-  var y = canvas.height - 30;
+  var y = canvas.height - 20;
   var dx = 2;
   var dy = -2;
 
-  var draw = function () {
-    // this clears the frame
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+  var drawBall = function () {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "#0095dd";
     ctx.fill();
     ctx.closePath();
+  };
+
+  var draw = function () {
+    // this clears the frame
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     x += dx;
     y += dy;
   };
