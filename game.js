352266c21d6f746358e0d7a8fc24ee241a995330
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-06 10:38:42 -0800

    initial commit 1/10
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

---
+++ game.js
@@ -0,0 +1,26 @@
+/*jslint browser:true, white:true*/
+(function () {
+  'use strict';
+
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.rect(20, 40, 50, 50);
+  ctx.fillStyle = "#ff0000";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.arc(240, 160, 40, 0, Math.PI * 2, false);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.rect(160, 10, 100, 40);
+  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  ctx.stroke();
+  ctx.closePath();
+
+}());
